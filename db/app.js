b517305e0d9cd787678fb8045b659e63884fb18e
Josh-Sharples <sharplesjd15@gmail.com>
2024-01-16 07:44:01 +0000

    This commit adds work completed for 03-get-api

--- db/app.js
+++ db/app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
-const { getTopics } = require('./Controllers/topics.controller')
+const { getTopics, getAllEndpoints } = require('./Controllers/topics.controller')
 
-app.use(express.json())
+app.get('/api/topics', getTopics);
 
-app.get('/api/topics', getTopics)
+app.get('/api', getAllEndpoints);
 
 app.all('*', (req, res) => {
   res.status(404).send({msg : 'Endpoint not found'})
